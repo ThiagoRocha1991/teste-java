0b7232f5c746fa32733f372e0ab54730bbf0b73d
ThiagoRocha1991 <37739685+ThiagoRocha1991@users.noreply.github.com>
2019-02-01 14:54:37 -0200

    Teste realizado por Thiago de Oliveira Rocha

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # teste-java
 Teste para recrutamento
+
+
+
+
+Jsoup
+
+O jsoup é uma biblioteca Java de código aberto projetada para analisar, extrair e manipular dados armazenados em documentos HTML.
+
+A versão do Jsoup utilizada no projeto foi a versão 1.11.3, que pode ser baixada através do link https://jsoup.org/download.
--- src/main/java/br/com/partnergroup/robo/test/app/App.java
+++ src/main/java/br/com/partnergroup/robo/test/app/App.java
@@ -1,54 +1,64 @@
 package br.com.partnergroup.robo.test.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import br.com.partnergroup.robo.test.bd.H2Server;
 
 public class App {
-	
-	
-	/*
-	 * Objetivo do teste:  obter o html de uma página, manipulá-lo e salvá-lo no banco de dados
-	 * 
-	 * 1) Obter o Html da página http://www.solhorticenter.com.br/ofertas/' e armazenar em variável
-	 * 2) Trabalhar o Html, removendo todas as tags (somente as tags, não o texto dentro delas). Usar expressões regulares
-	 * 3) Salvar o conteúdo do html na tabela tb_html (esta tabela tem 2 colunas: id int auto_increment e html varchar)
-	 */
-	
-	/*
-	 * Já esta incluso no código o servidor de banco de dados H2, que é iniciado junto com a aplicação;
-	 * É possível acessar o banco de dados via browser pelo endereço: http://localhost:9099 (somente enquanto a aplicação entiver
-	 * em execução), usando a string de conexão: jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1
-	 * É recomendável obter a pagina usando a biblioteca do apache hc: https://hc.apache.org/httpcomponents-client-4.5.x/index.html
-	 * Obs.: O banco de dados é em memória. Sempre que a aplicação terminar, os dados serão perdidos. 
-	 */
-	
 
 	public static void main(String[] args) throws SQLException, Exception {
-		try(H2Server server=new H2Server()){
+
+		// Declaração da String para acessar o site
+		String link = "http://www.solhorticenter.com.br/ofertas/";
+
+		// Declaração da String para acessar o banco
+		String url = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+
+		try (H2Server server = new H2Server()) {
+
 			server.start();
-			/*
-			 * String de conexão com o banco de dados: jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1
-			 * (Usuário 'sa' sem senha 
-			 */
-			
-			/*
-			 * 
-			 * Implementar a partir daqui!
-			 * 
-			 */
-			
-			
-			
-			
-			
-			
-			
-			JOptionPane.showMessageDialog(null, "Clique em ok para finalizar");
+
+			// Método Jsou.connect é responsável por acessar o link do site e salvar na
+			// varíavel doc do tipo Document usada para guardar o conteúdo html
+			Document doc = Jsoup.connect(link).get();
+
+			// Responsável por pegar os elementos dentro da tag <body>
+			Elements elementosTag = doc.select("body");
+
+			System.out.println("Elementos com a tag <body>:");
+
+			// for para percorrer todas as tags e um println para fins de teste no console
+			for (Element el : elementosTag)
+				System.out.println(el.text());
+
+			// Conversão do elementosTag para Texto para set no banco
+			String Texto = elementosTag.text().toString();
+			String validaTEXTO = Texto;
+
+			// bloco com try/catch para tratarmos as exceções
+			// conectamos com o banco de dados e preparamos a conexao para inserir no banco
+			try (Connection con = DriverManager.getConnection(url, "sa", "")) {
+				try (PreparedStatement ps = con.prepareStatement("INSERT INTO tb_html(html) VALUES(?)")) {
+					ps.setString(1, validaTEXTO);
+					ps.execute();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				JOptionPane.showMessageDialog(null, "Clique em ok para finalizar");
+
+			}
 		}
-
 	}
-
 }
